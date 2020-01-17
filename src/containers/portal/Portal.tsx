ac50835613170c175d781d5408d24d39f6bd2ce6
Murta Sanjuan <murta.sanjuan@gmail.com>
2020-01-17 13:12:51 +0100

    Update Portal.tsx

--- src/containers/portal/Portal.tsx
+++ src/containers/portal/Portal.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Book } from "../../types";
-import { getBooksList } from "../../redux/selectors";
 
-export function Portal(books: Book[]) {
-    return (<div>Portal</div>);
+export function Portal() {
+    return (
+        <div>
+        Portal
+
+      </div>
+    );
 }
-
-const mapStateToProps = (state: any) => {
-    const books: Book[] = getBooksList(state);
-    return { books };
-  };
-
-export default connect(
-    mapStateToProps,
-)(Portal);
