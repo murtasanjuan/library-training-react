38f83fd7ee539151f979f526a1bfa376856a20b9
Murta Sanjuan <murta.sanjuan@gmail.com>
2020-01-17 13:11:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import './App.scss';
-import Portal from './containers/portal/Portal';
+import './App.css';
+import { MainLayout } from './containers/main-layout/MainLayout';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Admin } from './containers/admin/Admin';
-import { Route, BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Route exact path='/' component={Portal} />
-        <Route path='/admin' component={Admin} />
-      </BrowserRouter>
-    </div>
+    
+    <BrowserRouter >
+    <Route exact path="/" component={MainLayout} />
+    <Route path="/admin" component={Admin} />
+  </BrowserRouter>
   );
-};
+}
 
 export default App;
